fe5f887cd0fa6c54c0022313fca8ba1975d757d4
unknown <melissikiss@gmail.com>
2021-05-27 17:37:09 -0700

    login page styles, needs media queries

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './pages/Login'; 
+import { Component } from 'react';
+
+function App() {
+  return (
+    <Login/>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Login.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../login.css'
+import loginImage from '../images/login-image.svg';
+
+
+
+const Login = () => {
+    return (
+        <div className="container">
+            <div className="row mt-5">
+                <div className="col">
+                    <img className="login-image" src={loginImage}></img>
+                </div>
+                <div className="col">
+                    <div>
+                        <h1 className="login-welcome">It's a Good Day to be Productive</h1>
+                        <form className="container login-form">
+                            <input placeholder="name" type="text" className="row" id="name-login"></input>
+                            <input placeholder="password" type="text" className="row" id="password-login"></input>
+                            <button type="submit" className="login-btn">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <hr className = "hr-line-login"></hr>
+            <div classname="">
+                <p>either information about the benefits of being productive or an about me section</p>
+            </div> 
+
+        </div>
+    )
+}
+
+export default Login;
