982ce6a023cd89690fb73118949eb1d9cc706093
melissikiss <melissikiss@gmail.com>
2021-05-31 14:36:09 -0700

    login page fully responsive

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,19 +8,19 @@ const Login = () => {
     return (
         <div className="container">
             <div className="row mt-5">
-                <div className="col">
+                <div className="col-sm-6">
                     <img className="login-image" src={loginImage}></img>
                 </div>
-                <div className="col">
-                    <div>
+                
+                    <div className="col-sm-6">
                         <h1 className="login-welcome">It's a Good Day to be Productive</h1>
-                        <form className="container login-form">
+                        <form className="login-form">
                             <input placeholder="name" type="text" className="row" id="name-login"></input>
                             <input placeholder="password" type="text" className="row" id="password-login"></input>
                             <button type="submit" className="login-btn">Login</button>
                         </form>
                     </div>
-                </div>
+                
             </div>
 
             <hr className = "hr-line-login"></hr>
